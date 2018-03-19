eac7eaabc5e1c315c376e00b9bf8c9b27e61f4fd
wilson <wilson.breiner16@gmail.com>
2018-03-19 13:05:57 +0800

    fix

--- app/User.php
+++ app/User.php
@@ -18,6 +18,9 @@ class User extends Authenticatable
      */    
     protected $fillable = [
         'auth0_id',
+        'first_name',
+        'last_name',
+        'email',
         'address',
         'permission',
         'visited_count',
