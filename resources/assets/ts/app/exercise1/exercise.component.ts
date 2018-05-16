a6d750bd676e6ac61801b49366cd1320d4663c39
gullocean <gullocean@gmail.com>
2018-05-16 06:29:03 -0300

    add timers & selection in the list table

--- resources/assets/ts/app/exercise1/exercise.component.ts
+++ resources/assets/ts/app/exercise1/exercise.component.ts
@@ -3,8 +3,8 @@ import { Router, ActivatedRoute, ActivationEnd  } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { TranslateService } from '@ngx-translate/core';
-import { Observable } from 'rxjs/Rx';
-import { map } from 'lodash';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { isEmpty, map } from 'lodash';
 
 import { routerTransition } from '../router.animations';
 import { GlobalService } from '../shared/services/global.service';
@@ -34,6 +34,11 @@ export class ExerciseComponent implements OnInit, OnDestroy {
   offset: number = 0;
   count: number = 0;
   type: string = '';
+  currentQuiz: Problem;
+  remainingTime: number = 0;
+  preTimerSubscription: Subscription;
+  mainTimerSubscription: Subscription;
+  step: number = 0;
   // currentProblem: Problem;
   // currentAnswer: Answer;
   // currentpretime: number;
@@ -153,6 +158,10 @@ export class ExerciseComponent implements OnInit, OnDestroy {
         ({ data, recordsFiltered, recordsTotal }) => {
           this.list = map(data, (d: Problem, index: number) => ({ ...d, no: index + this.offset * this.limit + 1 }));
           this.count = recordsTotal;
+
+          if (this.list.length > 0 && isEmpty(this.currentQuiz)) {
+            this.selectQuiz(this.list[0]);
+          }
         }
       );
   }
@@ -162,5 +171,86 @@ export class ExerciseComponent implements OnInit, OnDestroy {
     this.offset = page.offset;
 
     this.getQuizList(this.type, this.offset, this.limit);
+  }
+
+  getTypes(category: string) {
+    return this.globalService.problemTypes[category];
+  }
+
+  getTypeName(category: string, value: string) {
+    const arr_types: { value: string, title: string }[] = this.getTypes(category);
+
+    if(arr_types != null) {
+      for (let i = 0;  i < arr_types.length; i++) {
+        if (arr_types[i].value == value)
+          return arr_types[i].title;
+      }
+    }
+
+    return '';
+  }
+
+  selectQuizRow(row: { selected: Problem[] }) {
+    if (row.selected.length > 0) {
+      this.selectQuiz(row.selected[0]);
+    }
+  }
+
+  selectQuiz(quiz: Problem) {
+    const { preparation_time, limit_time, id } = quiz;
+
+    if (!isEmpty(this.currentQuiz) && id === this.currentQuiz.id) {
+      return;
+    }
+
+    this.currentQuiz = { ...quiz };
+
+    this.startExercise();
+  }
+
+  startExercise() {
+    if (isEmpty(this.currentQuiz)) return;
+
+    const { preparation_time, limit_time, id } = this.currentQuiz;
+
+    this.stopTimer(this.preTimerSubscription);
+    this.stopTimer(this.mainTimerSubscription);
+
+    this.step = 0;
+    this.remainingTime = preparation_time;
+
+    this.preTimerSubscription = this.startTimer(0, (t: number) => {
+      this.remainingTime = preparation_time - t;
+
+      if (this.remainingTime <= 0) {
+        this.step = 1;
+        this.remainingTime = limit_time;
+        this.stopTimer(this.preTimerSubscription);
+
+        this.mainTimerSubscription = this.startTimer(preparation_time, (t: number) => {
+          this.remainingTime = limit_time - t;
+
+          if (this.remainingTime <= 0) {
+            this.step = 2;
+            this.remainingTime = 0;
+            this.stopTimer(this.mainTimerSubscription);
+          }
+        });
+      }
+    });
+  }
+
+  startTimer(delay: number, func: Function): Subscription {
+    const timer = Observable.timer(delay, 1000);
+    const subscription = timer.subscribe(t => {
+      func(t);
+    });
+
+    return subscription;
+  }
+
+  stopTimer(subscription: Subscription) {
+    if (subscription === undefined) return;
+    subscription.unsubscribe();
   }
 }
